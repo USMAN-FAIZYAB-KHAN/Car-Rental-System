da527117cd3667cc08b9a61dc3cc7eb54069719a
Muhammad-Zunain <muhammadzunain2004@gmail.com>
2024-06-05 15:08:16 +0500

    Create Models

--- car_rental_system/car_rental_system/settings.py
+++ car_rental_system/car_rental_system/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'car_rental_system.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'test_db',
+        'NAME': 'testdb',
         'USER': 'root',
         'PASSWORD': '12345678',
         'HOST': 'localhost',
