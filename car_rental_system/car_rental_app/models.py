75cdfc3566e97a0a2a473ccf00126a37479b476c
USMAN-FAIZYAB-KHAN <usmanfaizyab123@gmail.com>
2024-06-15 14:21:46 +0500

    added models and changed settings

--- car_rental_system/car_rental_app/models.py
+++ car_rental_system/car_rental_app/models.py
@@ -2,10 +2,10 @@ from django.db import models
 
 # Create your models here.
 class Car(models.Model):
-    car_id = models.AutoField(primary_key=True)
-    car_name = models.CharField(max_length=50)
-    car_type = models.CharField(max_length=50)
-    car_price = models.DecimalField(max_digits=10, decimal_places=2)
-    
+    name = models.CharField(max_length=100)
+    price = models.IntegerField()
+    description = models.TextField()
+    available = models.BooleanField(default=True)
+
     def __str__(self):
-        return self.car_name
+        return self.name
--- car_rental_system/car_rental_system/settings.py
+++ car_rental_system/car_rental_system/settings.py
@@ -14,7 +14,8 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+TEMPLATE_DIR = BASE_DIR / 'templates'
+STATIC_DIR = BASE_DIR / 'static'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
@@ -55,7 +56,7 @@ ROOT_URLCONF = 'car_rental_system.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATE_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -77,9 +78,9 @@ WSGI_APPLICATION = 'car_rental_system.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'test_db',
-        'USER': 'root',
-        'PASSWORD': '12345678',
+        'NAME': 'car_rental_db',
+        'USER': 'django_project_user',
+        'PASSWORD': 'P@ssword123',
         'HOST': 'localhost',
         'PORT': '3306',
     }
@@ -121,6 +122,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [STATIC_DIR]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
