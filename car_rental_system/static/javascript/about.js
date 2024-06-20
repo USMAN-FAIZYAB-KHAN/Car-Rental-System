62b31ead4858b5696236765741d55c42fa99ee34
ayankhan2021 <148139254+ayankhan2021@users.noreply.github.com>
2024-06-20 14:25:32 +0500

    Add files via upload
    about-scripting added..

---
+++ car_rental_system/static/javascript/about.js
@@ -0,0 +1,89 @@
+function countUp(counter, targetNumber, duration) {
+    let startNumber = 0;
+    const increment = targetNumber / (duration / 10);
+
+    function updateCounter() {
+        startNumber += increment;
+        if (startNumber < targetNumber) {
+            counter.innerText = Math.ceil(startNumber);
+            setTimeout(updateCounter, 10);
+        } else {
+            counter.innerText = targetNumber;
+        }
+    }
+
+    updateCounter();
+}
+
+const counters = document.getElementsByClassName("counter");
+for (let i = 0; i < counters.length; i++) {
+    const targetNumber = counters[i].innerText;
+    countUp(counters[i], targetNumber, 2000);
+}
+
+
+
+const observer = new IntersectionObserver((entries)=> {
+    entries.forEach((entry) => {
+        // console.log(entry)
+        if (entry.isIntersecting){
+            entry.target.classList.add('show')       
+        }else {
+            entry.target.classList.remove('show')
+        }
+    })
+})
+const observer2 = new IntersectionObserver((entries)=> {
+    entries.forEach((entry) => {
+        // console.log(entry)
+        if (entry.isIntersecting){
+            entry.target.classList.add('show')       
+        }else {
+            entry.target.classList.remove('show')
+        }
+    })
+})
+const observer3 = new IntersectionObserver((entries)=> {
+    entries.forEach((entry) => {
+        // console.log(entry)
+        if (entry.isIntersecting){
+            entry.target.classList.add('show-down')       
+        }else {
+            entry.target.classList.remove('show-down')
+        }
+    })
+})
+
+
+
+
+const hiddenElements = document.querySelectorAll('.hidden-left')
+hiddenElements.forEach((el) => observer.observe(el))
+const hiddenElements2 = document.querySelectorAll('.hidden-right')
+hiddenElements2.forEach((el) => observer2.observe(el))
+const hiddenElements3 = document.querySelectorAll('.hidden-down')
+hiddenElements3.forEach((el) => observer3.observe(el))
+
+
+
+
+function to_luxury() {
+    document.querySelector('.p-tag').innerText = `We offer a meticulously curated collection of the most sought-after luxury vehicles on the market.
+                    Whether you prefer the sporty allure of a high-performance sports car, the sophistication of a sleek
+                    and luxurious sedan, or the versatility of a premium SUV, we have the perfect car to match your
+                    discerning taste.`
+}
+function to_prestige() {
+    document.querySelector('.p-tag').innerText = `We understand that prestige goes beyond luxury. It's about making a statement, embracing sophistication, and indulging in the finer things in life. That's why we offer an exclusive selection of prestigious cars that exude elegance, style, and status.`
+}
+function to_comfort() {
+    document.querySelector('.p-tag').innerText = `We prioritize your comfort and convenience throughout your journey. We understand that a comfortable ride can make a world of difference, whether you're embarking on a business trip or enjoying a leisurely vacation. That's why we offer a wide range of well-maintained, comfortable cars that cater to your specific needs.`
+}
+
+
+
+
+
+
+
+
