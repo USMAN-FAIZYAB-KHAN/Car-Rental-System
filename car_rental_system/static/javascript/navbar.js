abc83c331e54fbec1a3424ede3912240de5e53db
Muhammad-Zunain <muhammadzunain2004@gmail.com>
2024-06-20 14:28:48 +0500

    Fix error in Navbar

--- car_rental_system/static/javascript/navbar.js
+++ car_rental_system/static/javascript/navbar.js
@@ -11,30 +11,19 @@ const moveSelector = (target) => {
   let height = rect.height;
   let width = rect.width;
 
-  movingSelector.style.top = `${top}px`;
-  movingSelector.style.left = `${left}px`;
-  movingSelector.style.height = `${height}px`;
-  movingSelector.style.width = `${width}px`;
+  // setTimeout(()=>{
+    movingSelector.style.top = `${top}px`;
+    movingSelector.style.left = `${left}px`;
+    movingSelector.style.height = `${height}px`;
+    movingSelector.style.width = `${width}px`;
 
+  // },300)
 };
 
-navLinks.forEach((navLink) => {
-  navLink.addEventListener("click", (e) => {
-    e.preventDefault();
-    let target = e.currentTarget;
-
-    navLinks.forEach((link) => {
-      link.classList.remove("active");
-    });
-
-    target.classList.add("active");
-    moveSelector(target);
+document.addEventListener('DOMContentLoaded',()=>{
+  navLinks.forEach((navLink) => {
+    if (navLink.classList.contains('active')) {
+      moveSelector(navLink);
+    }
   });
-});
-
-window.addEventListener("DOMContentLoaded", () => {
-  const activeLink = document.querySelector(".nav__link.active");
-  if (activeLink) {
-    moveSelector(activeLink);
-  }
 });
