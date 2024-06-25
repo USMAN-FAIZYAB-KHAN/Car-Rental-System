d62a391c4ae12230156135e213684d56205bc53a
USMAN-FAIZYAB-KHAN <usmanfaizyab123@gmail.com>
2024-06-25 11:13:31 +0500

    Added filter for price and category

--- car_rental_system/car_rental_app/models.py
+++ car_rental_system/car_rental_app/models.py
@@ -14,6 +14,14 @@ class User(models.Model):
 
     def __str__(self):
         return self.firstname + ' ' + self.lastname
+    
+class UserType(models.Model):
+    type_id = models.AutoField(primary_key=True)
+    type_name = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.type_name
+    
 
 class Category(models.Model):
     category_id = models.AutoField(primary_key=True)
@@ -21,36 +29,69 @@ class Category(models.Model):
     seating_capacity = models.IntegerField()
     description = models.TextField()
 
-
-class Car(models.Model):
-    category = models.ForeignKey(Category, on_delete= models.SET_NULL, null=True)
-    car_id = models.AutoField(primary_key=True)
+    def __str__(self):
+        return self.name
+    
+class Transmission(models.Model):
+    transmission_id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=50)
-    color = models.CharField(max_length=10)
-    transmission_type = models.CharField(max_length=10, choices=[('Manual', 'Manual'),('Automatic', 'Automatic')])
-    price_per_day = models.FloatField()
-    license_number = models.CharField(max_length=15)
-    modal = models.IntegerField()
+    description = models.TextField(null=True)
 
     def __str__(self):
-        return self.car_name
+        return self.name
+    
+class Model(models.Model):
+    model_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=50)
+    manufacturer = models.CharField(max_length=50)
+    transmission = models.ForeignKey(Transmission, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Car(models.Model):
+    car_id = models.AutoField(primary_key=True)
+    category = models.ForeignKey(Category, on_delete= models.SET_NULL, null=True)
+    model = models.ForeignKey(Model, on_delete=models.SET_NULL, null=True)
+    year = models.IntegerField()
+    color = models.CharField(max_length=10)
+    price_per_day = models.FloatField()
+    license_number = models.CharField(max_length=15)
+    availability = models.BooleanField()
+
+    def __str__(self):
+        return self.license_number
+    
+
+class RentalStatus(models.Model):
+    status_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=50)
+    description = models.TextField(null=True)
+
+    def __str__(self):
+        return self.name
+
 
 class Rental(models.Model):
     user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     car = models.ForeignKey(Car, on_delete=models.SET_NULL, null=True)
-    rental_id = models.AutoField(primary_key=True)
     booking_date = models.DateField()
     pickup_date = models.DateField()
     drop_date = models.DateField()
     total_cost = models.DecimalField(max_digits=10, decimal_places=2)
-    status = models.CharField(max_length=10, choices=[('Completed', 'Completed'), ('Approved', 'Approved'), ('Ongoing', 'Ongoing')])
+    status = models.ForeignKey(RentalStatus, on_delete=models.SET_NULL, null=True)
+
+    class Meta:
+            unique_together = ('user', 'pickup_date')
 
 class Payment(models.Model):
     rental = models.ForeignKey(Rental, on_delete=models.SET_NULL, null=True)
     payment_number = models.IntegerField()
-    payment_menthod = models.CharField(max_length=50)
+    payment_method = models.CharField(max_length=50)
     payment_date = models.DateField()
     amount = models.DecimalField(max_digits=10, decimal_places=2)
+    
 
 
 class Review(models.Model):
