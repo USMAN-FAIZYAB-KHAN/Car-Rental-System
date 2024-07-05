380e1198d7f946dbcd951d66371a0dcff8707b2d
Muhammad-Zunain <muhammadzunain2004@gmail.com>
2024-07-05 17:29:51 +0500

    Add Email funtionality in JS

--- car_rental_system/static/javascript/contact.js
+++ car_rental_system/static/javascript/contact.js
@@ -1,41 +1,41 @@
 const testimonials = [
-    {
-      name: "Eva Sawyer",
-      job: "CEO, Fashworks",
-      image: "../static/images/assets/person__1.jpg",
-      testimonial:
-        "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Obcaecati, tempore!",
-    },
-    {
-      name: "Katey Topaz",
-      job: "Developer, TechCrew",
-      image: "../static/images/assets/person__2.jpg",
-      testimonial:
-        "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Obcaecati, tempore!",
-    },
-    {
-      name: "Jae Robin",
-      job: "UI Designer, Affinity Agency",
-      image: "../static/images/assets/person__3.jpg",
-      testimonial:
-        "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Obcaecati, tempore!",
-    },
-    {
-      name: "Nicola Blakely",
-      job: "CEO,Zeal Wheels",
-      image: "../static/images/assets/person__4.jpg",
-      testimonial:
-        "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Obcaecati, tempore!",
-    },
-  ];
-  
-  let currentIndex = 0;
-  const testimonialContainer = document.getElementById("testimonial-container");
-  const nextBtn = document.getElementById("next");
-  const prevBtn = document.getElementById("prev");
-  
-  const createTestimonialHTML = (testimonial) => {
-    return `
+  {
+    name: "Eva Sawyer",
+    job: "CEO, Fashworks",
+    image: "../static/images/assets/person__1.jpg",
+    testimonial:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Obcaecati, tempore!",
+  },
+  {
+    name: "Katey Topaz",
+    job: "Developer, TechCrew",
+    image: "../static/images/assets/person__2.jpg",
+    testimonial:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Obcaecati, tempore!",
+  },
+  {
+    name: "Jae Robin",
+    job: "UI Designer, Affinity Agency",
+    image: "../static/images/assets/person__3.jpg",
+    testimonial:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Obcaecati, tempore!",
+  },
+  {
+    name: "Nicola Blakely",
+    job: "CEO,Zeal Wheels",
+    image: "../static/images/assets/person__4.jpg",
+    testimonial:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Obcaecati, tempore!",
+  },
+];
+
+let currentIndex = 0;
+const testimonialContainer = document.getElementById("testimonial-container");
+const nextBtn = document.getElementById("next");
+const prevBtn = document.getElementById("prev");
+
+const createTestimonialHTML = (testimonial) => {
+  return `
       <div class="testimonial">
         <p>${testimonial.testimonial}</p>
         <img src=${testimonial.image} alt="${testimonial.name}">
@@ -43,73 +43,80 @@ const testimonials = [
         <h6>${testimonial.job}</h6>
       </div>
     `;
-  };
-  
-  const updateTestimonials = () => {
-    const nextIndex = (currentIndex + 1) % testimonials.length;
-    const prevIndex = (currentIndex - 1 + testimonials.length) % testimonials.length;
-  
-    testimonialContainer.innerHTML = `
+};
+
+const updateTestimonials = () => {
+  const nextIndex = (currentIndex + 1) % testimonials.length;
+  const prevIndex =
+    (currentIndex - 1 + testimonials.length) % testimonials.length;
+
+  testimonialContainer.innerHTML = `
       ${createTestimonialHTML(testimonials[prevIndex])}
       ${createTestimonialHTML(testimonials[currentIndex])}
       ${createTestimonialHTML(testimonials[nextIndex])}
     `;
-    
-    testimonialContainer.style.transform = 'translateX(-100%)';
-  };
-  
-  const slideNext = () => {
-    testimonialContainer.style.transition = 'transform 0.5s ease-in-out';
-    testimonialContainer.style.transform = 'translateX(-200%)';
-  
-    testimonialContainer.addEventListener('transitionend', () => {
+
+  testimonialContainer.style.transform = "translateX(-100%)";
+};
+
+const slideNext = () => {
+  testimonialContainer.style.transition = "transform 0.5s ease-in-out";
+  testimonialContainer.style.transform = "translateX(-200%)";
+
+  testimonialContainer.addEventListener(
+    "transitionend",
+    () => {
       currentIndex = (currentIndex + 1) % testimonials.length;
       updateTestimonials();
-      testimonialContainer.style.transition = 'none';
-      testimonialContainer.style.transform = 'translateX(-100%)';
-    }, { once: true });
-  };
-  
-  const slidePrev = () => {
-    testimonialContainer.style.transition = 'transform 0.5s ease-in-out';
-    testimonialContainer.style.transform = 'translateX(0)';
-  
-    testimonialContainer.addEventListener('transitionend', () => {
-      currentIndex = (currentIndex - 1 + testimonials.length) % testimonials.length;
+      testimonialContainer.style.transition = "none";
+      testimonialContainer.style.transform = "translateX(-100%)";
+    },
+    { once: true }
+  );
+};
+
+const slidePrev = () => {
+  testimonialContainer.style.transition = "transform 0.5s ease-in-out";
+  testimonialContainer.style.transform = "translateX(0)";
+
+  testimonialContainer.addEventListener(
+    "transitionend",
+    () => {
+      currentIndex =
+        (currentIndex - 1 + testimonials.length) % testimonials.length;
       updateTestimonials();
-      testimonialContainer.style.transition = 'none';
-      testimonialContainer.style.transform = 'translateX(-100%)';
-    }, { once: true });
-  };
-  
-  nextBtn.addEventListener("click", slideNext);
-  prevBtn.addEventListener("click", slidePrev);
-  
-  updateTestimonials();
-  testimonialContainer.style.transform = 'translateX(-100%)';
-  testimonialContainer.style.transition = 'none';
+      testimonialContainer.style.transition = "none";
+      testimonialContainer.style.transform = "translateX(-100%)";
+    },
+    { once: true }
+  );
+};
 
+nextBtn.addEventListener("click", slideNext);
+prevBtn.addEventListener("click", slidePrev);
 
+updateTestimonials();
+testimonialContainer.style.transform = "translateX(-100%)";
+testimonialContainer.style.transition = "none";
 
-  const observer = new IntersectionObserver((entries)=> {
-    entries.forEach((entry) => {
-        // console.log(entry)
-        if (entry.isIntersecting){
-            entry.target.classList.add('show')       
-        }else {
-            entry.target.classList.remove('show')
-        }
-    })
-})
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    // console.log(entry)
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    } else {
+      entry.target.classList.remove("show");
+    }
+  });
+});
 
-const hiddenElements = document.querySelectorAll('.hidden')
-hiddenElements.forEach((el) => observer.observe(el))
+const hiddenElements = document.querySelectorAll(".hidden");
+hiddenElements.forEach((el) => observer.observe(el));
 
 const showToast = (border) => {
   toast_notification.classList.remove("toastanimate", "toastanimateout");
-  void toast_notification.offsetWidth; 
+  void toast_notification.offsetWidth;
 
-  
   border.classList.add("toastBoderAnimation");
 
   toast_notification.classList.add("toastanimate");
@@ -119,8 +126,8 @@ const showToast = (border) => {
     toast_notification.classList.add("toastanimateout");
     setTimeout(() => {
       toast_notification.classList.remove("toastanimateout");
-    }, 300); 
-  }, 2300); 
+    }, 300);
+  }, 2300);
 };
 
 const toast_notification = document.querySelector("#toast_notification");
@@ -130,13 +137,45 @@ checkButton.addEventListener("click", (e) => {
   let name = document.querySelector("#name").value;
   let email = document.querySelector("#email").value;
   let number = document.querySelector("#phonenumber").value;
+  let message = document.querySelector(".message").value;
+  console.log(email, number, message);
+
   let toast_border = document.querySelector("#toast-border");
 
+  const params = {
+    user_name: name,
+    ph_no: number,
+    user_email: email,
+    message: message,
+  };
   e.preventDefault();
-  if (name === "" || email === "" || number === "") {
+  if (name === "" || email === "" || number === "" || message === "") {
     toast_border.classList.remove("toastBoderAnimation");
     showToast(toast_border);
   }
-  
-  // location.reload()
+  emailjs
+    .send("service_a4wsrkd", "template_4f02w0v", params)
+    .then(function (res) {
+    });
 });
+
+// function sendMail() {
+//   var params = {
+//     user_name: document.querySelector('.user_name').value,
+//     ph_no: document.querySelector('.ph_no').value,
+//     user_email: document.querySelector('.user_email').value,
+//     message: document.querySelector('.message').value,
+//   }
+
+// }
+
+// function showPopup() {
+//   var popup = document.getElementById('popup');
+//   popup.classList.remove('hidden2');
+//   popup.classList.add('visible');
+
+//   setTimeout(function() {
+//     popup.classList.remove('visible');
+//     popup.classList.add('hidden2');
+//   }, 2000);
+// }
