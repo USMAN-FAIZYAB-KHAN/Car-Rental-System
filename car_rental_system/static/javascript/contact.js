1dfbb9eb9260f2f7bef0017070a2b0e0f1068404
Muhammad-Zunain <muhammadzunain2004@gmail.com>
2024-07-08 11:23:59 +0500

    Add payement template

--- car_rental_system/static/javascript/contact.js
+++ car_rental_system/static/javascript/contact.js
@@ -1,4 +1,7 @@
-const testimonials = [
+// <------------------------------ Testimonial ------------------------------>
+
+// Define the array of testimonials with their details
+const testimonials = [              
   {
     name: "Eva Sawyer",
     job: "CEO, Fashworks",
@@ -29,11 +32,13 @@ const testimonials = [
   },
 ];
 
+// Initialize the current index to 0 and set up the event listeners for the next and previous buttons
 let currentIndex = 0;
 const testimonialContainer = document.getElementById("testimonial-container");
 const nextBtn = document.getElementById("next");
 const prevBtn = document.getElementById("prev");
 
+// Function to create the HTML structure for a testimonial
 const createTestimonialHTML = (testimonial) => {
   return `
       <div class="testimonial">
@@ -45,10 +50,10 @@ const createTestimonialHTML = (testimonial) => {
     `;
 };
 
+// Function to update the testimonials displayed
 const updateTestimonials = () => {
   const nextIndex = (currentIndex + 1) % testimonials.length;
-  const prevIndex =
-    (currentIndex - 1 + testimonials.length) % testimonials.length;
+  const prevIndex = (currentIndex - 1 + testimonials.length) % testimonials.length;
 
   testimonialContainer.innerHTML = `
       ${createTestimonialHTML(testimonials[prevIndex])}
@@ -59,6 +64,7 @@ const updateTestimonials = () => {
   testimonialContainer.style.transform = "translateX(-100%)";
 };
 
+// Function to slide to the next testimonial
 const slideNext = () => {
   testimonialContainer.style.transition = "transform 0.5s ease-in-out";
   testimonialContainer.style.transform = "translateX(-200%)";
@@ -75,6 +81,7 @@ const slideNext = () => {
   );
 };
 
+// Function to slide to the previous testimonial
 const slidePrev = () => {
   testimonialContainer.style.transition = "transform 0.5s ease-in-out";
   testimonialContainer.style.transform = "translateX(0)";
@@ -82,8 +89,7 @@ const slidePrev = () => {
   testimonialContainer.addEventListener(
     "transitionend",
     () => {
-      currentIndex =
-        (currentIndex - 1 + testimonials.length) % testimonials.length;
+      currentIndex = (currentIndex - 1 + testimonials.length) % testimonials.length;
       updateTestimonials();
       testimonialContainer.style.transition = "none";
       testimonialContainer.style.transform = "translateX(-100%)";
@@ -92,16 +98,18 @@ const slidePrev = () => {
   );
 };
 
+// Add event listeners for next and previous buttons
 nextBtn.addEventListener("click", slideNext);
 prevBtn.addEventListener("click", slidePrev);
 
+// Initial call to update testimonials
 updateTestimonials();
 testimonialContainer.style.transform = "translateX(-100%)";
 testimonialContainer.style.transition = "none";
 
+// Intersection Observer to handle element visibility
 const observer = new IntersectionObserver((entries) => {
   entries.forEach((entry) => {
-    // console.log(entry)
     if (entry.isIntersecting) {
       entry.target.classList.add("show");
     } else {
@@ -110,10 +118,18 @@ const observer = new IntersectionObserver((entries) => {
   });
 });
 
+// Observe elements with the class "hidden"
 const hiddenElements = document.querySelectorAll(".hidden");
 hiddenElements.forEach((el) => observer.observe(el));
 
-const showToast = (border) => {
+// <------------------------------ Thoast Notification ------------------------------>
+
+// showToast Function ( Display Different Thoast according to the input field )
+
+const showToast = (message, type) => {
+  let border = document.querySelector("#toast-border");
+  border.classList.remove("toastBoderAnimation");
+
   toast_notification.classList.remove("toastanimate", "toastanimateout");
   void toast_notification.offsetWidth;
 
@@ -121,61 +137,71 @@ const showToast = (border) => {
 
   toast_notification.classList.add("toastanimate");
 
+  // Update the toast message and icon
+  toast_notification.querySelector('.toast__message h4').innerText = type;
+  toast_notification.querySelector('.toast__message p').innerText = message;
+
+  if (type === 'Success') {
+    toast_notification.querySelector('.toast__link i').classList.remove('fa-xmark');
+    toast_notification.querySelector('.toast__link i').classList.add('fa-check');
+  } else {
+    toast_notification.querySelector('.toast__link i').classList.remove('fa-check');
+    toast_notification.querySelector('.toast__link i').classList.add('fa-xmark');
+  }
+
+  toast_notification.classList.add('show');
+
   setTimeout(() => {
     toast_notification.classList.remove("toastanimate");
     toast_notification.classList.add("toastanimateout");
     setTimeout(() => {
       toast_notification.classList.remove("toastanimateout");
+      toast_notification.classList.remove('show');
     }, 300);
   }, 2300);
 };
 
-const toast_notification = document.querySelector("#toast_notification");
+// Email Validate the inputs
+const isValidEmail = (email) => {
+  const emailRegex = /^[^\s@]+@gmail\.com$/;
+  return emailRegex.test(email);
+};
+
+const toast_notification = document.querySelector("#toast_notification");   //   thoastNotification Container
 const checkButton = document.querySelector("#btn");
 
-checkButton.addEventListener("click", (e) => {
+checkButton.addEventListener("click", (e) => {                              // Apply the click event to the button and the toast container 
+  e.preventDefault();
+
   let name = document.querySelector("#name").value;
   let email = document.querySelector("#email").value;
   let number = document.querySelector("#phonenumber").value;
   let message = document.querySelector(".message").value;
-  console.log(email, number, message);
-
-  let toast_border = document.querySelector("#toast-border");
-
+  
   const params = {
     user_name: name,
     ph_no: number,
     user_email: email,
     message: message,
   };
-  e.preventDefault();
-  if (name === "" || email === "" || number === "" || message === "") {
-    toast_border.classList.remove("toastBoderAnimation");
-    showToast(toast_border);
+  
+  if (name === "" || email === "" || number === "" || message === "") {    // Check empty string 
+    showToast("Fill out all the input fields", "Error");
+  } else if (!isValidEmail(email)) {
+    showToast("Please enter a valid Gmail address.", "Error");
+  } else if (number.length !== 11) {
+    showToast("Phone number must be 11 digits.", "Error");
+  } else {
+    showToast("Your message was successfully sent.", "Success");
+    
+    // Clear the input fields
+    document.querySelector("#name").value = "";
+    document.querySelector("#email").value = "";
+    document.querySelector("#phonenumber").value = "";
+    document.querySelector(".message").value = "";
+    
+    // Send Email Function (Using EmailJS)
+    emailjs.send("service_a4wsrkd", "template_4f02w0v", params)
+    .then(function (res) {});
   }
-  emailjs
-    .send("service_a4wsrkd", "template_4f02w0v", params)
-    .then(function (res) {
-    });
 });
-
-// function sendMail() {
-//   var params = {
-//     user_name: document.querySelector('.user_name').value,
-//     ph_no: document.querySelector('.ph_no').value,
-//     user_email: document.querySelector('.user_email').value,
-//     message: document.querySelector('.message').value,
-//   }
-
-// }
-
-// function showPopup() {
-//   var popup = document.getElementById('popup');
-//   popup.classList.remove('hidden2');
-//   popup.classList.add('visible');
-
-//   setTimeout(function() {
-//     popup.classList.remove('visible');
-//     popup.classList.add('hidden2');
-//   }, 2000);
-// }
